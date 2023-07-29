980dcde350dc8c02f4b3cc8d06a898a4b331377d
ZaPhat <93189118+Zaphat@users.noreply.github.com>
2023-07-29 21:19:47 +0700

    v 1.1.1 mid version (incomplete)

--- client.py
+++ client.py
@@ -1,17 +1,139 @@
 
 from PySide6.QtWidgets import (QApplication, QLineEdit, QPlainTextEdit, QPushButton,
-                               QScrollArea, QSizePolicy, QTextBrowser, QWidget)
+                               QScrollArea, QSizePolicy, QTextBrowser, QWidget, QLabel)
 from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
                            QFont, QFontDatabase, QGradient, QIcon,
                            QImage, QKeySequence, QLinearGradient, QPainter,
-                           QPalette, QPixmap, QRadialGradient, QTransform)
+                           QPalette, QPixmap, QRadialGradient, QTransform, QIntValidator)
 from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
                             QMetaObject, QObject, QPoint, QRect,
                             QSize, QTime, QUrl, Qt, QEvent)
+
 import sys
 import socket
 import threading
 import ctypes
+
+
+# ---------------------------------------------------------------Connect Form---------------------------------------------------------------
+
+
+class Form(object):
+    def setupUi(self, Widget):
+        if not Widget.objectName():
+            Widget.setObjectName(u"Widget")
+
+        Widget.resize(422, 325)
+
+        self.port_input = QLineEdit(Widget)
+        self.port_input.setObjectName(u"port_input")
+        self.port_input.setGeometry(QRect(150, 140, 161, 51))
+        sizePolicy = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(
+            self.port_input.sizePolicy().hasHeightForWidth())
+        self.port_input.setSizePolicy(sizePolicy)
+        self.port_input.setStyleSheet(u"text-align: center; \n"
+                                      "vertical-align: center;")
+
+        font = QFont()
+        font.setPointSize(14)
+        font.setBold(True)
+        self.port_input.setFont(font)
+
+        self.host_input = QLineEdit(Widget)
+        self.host_input.setObjectName(u"host_input")
+        self.host_input.setGeometry(QRect(150, 50, 241, 51))
+        sizePolicy.setHeightForWidth(
+            self.host_input.sizePolicy().hasHeightForWidth())
+        self.host_input.setSizePolicy(sizePolicy)
+        self.host_input.setFont(font)
+        self.host_input.setStyleSheet(u"text-align: center; \n"
+                                      "vertical-align: center;")
+
+        self.label = QLabel(Widget)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(30, 60, 111, 31))
+
+        font1 = QFont()
+        font1.setPointSize(20)
+        font1.setBold(True)
+
+        self.label.setFont(font1)
+        self.label_2 = QLabel(Widget)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setGeometry(QRect(30, 150, 81, 31))
+
+        font2 = QFont()
+        font2.setPointSize(18)
+        font2.setBold(True)
+        self.label_2.setFont(font2)
+
+        self.pushButton = QPushButton(Widget)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setGeometry(QRect(140, 250, 141, 41))
+        sizePolicy.setHeightForWidth(
+            self.pushButton.sizePolicy().hasHeightForWidth())
+        self.pushButton.setSizePolicy(sizePolicy)
+        self.pushButton.setStyleSheet(u"text-align: center; \n"
+                                      "vertical-align: center;")
+        self.pushButton.setFont(font)
+        self.pushButton.setText(
+            QCoreApplication.translate("Widget", u"Connect", None))
+
+        self.retranslateUi(Widget)
+        QMetaObject.connectSlotsByName(Widget)
+
+    def retranslateUi(self, Widget):
+        Widget.setWindowTitle(
+            QCoreApplication.translate("Widget", u"Connect to server", None))
+        self.host_input.setText("")
+        self.label.setText(QCoreApplication.translate("Widget", u"HOST", None))
+        self.label_2.setText(
+            QCoreApplication.translate("Widget", u"PORT", None))
+
+
+class Login(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.ui = Form()
+        self.ui.setupUi(self)
+        self.ui.host_input.setFocus()
+
+        # set criteria for input area
+        self.ui.pushButton.setEnabled(0)
+        self.ui.pushButton.clicked.connect(self.connect)
+
+        self.ui.host_input.textChanged.connect(lambda:  self.ui.pushButton.setEnabled(
+            self.ui.host_input.text() != "" and self.ui.port_input.text() != ""))
+        self.ui.host_input.returnPressed.connect(
+            lambda: self.ui.port_input.setFocus())
+        self.ui.host_input.setAlignment(Qt.AlignCenter)
+
+        self.ui.port_input.textChanged.connect(lambda: self.ui.pushButton.setEnabled(
+            self.ui.host_input.text() != "" and self.ui.port_input.text() != ""))
+        self.ui.port_input.setValidator(QIntValidator(0, 65535))
+        self.ui.port_input.returnPressed.connect(
+            lambda: self.ui.pushButton.click() if self.ui.pushButton.isEnabled() else None)
+        self.ui.port_input.setAlignment(Qt.AlignCenter)
+
+        self.ui.host_input.setTabOrder(self.ui.host_input, self.ui.port_input)
+        self.ui.port_input.setTabOrder(self.ui.port_input, self.ui.pushButton)
+        # restrict resizing windows
+        self.setFixedSize(self.size())
+
+    def connect(self):
+        try:
+            client.connect((self.ui.host_input.text(),
+                           int(self.ui.port_input.text())))
+            print("Connected successfully")
+            self.close()
+        except:
+            ctypes.windll.user32.MessageBoxW(
+                0, "Cannot connect to the server", "Error", 0)
+
+
 # ----------------------------------------------------------Graphic User Interface----------------------------------------------------------
 
 
@@ -24,28 +146,28 @@ class Ui_Widget(object):
         Widget.resize(800, 600)
         self.plainTextEdit = QPlainTextEdit(Widget)
         self.plainTextEdit.setObjectName(u"plainTextEdit")
-        self.plainTextEdit.setGeometry(QRect(10, 530, 651, 61))
+        self.plainTextEdit.setGeometry(QRect(10, 530, 650, 50))
         Ui_Widget.component.append(self.plainTextEdit)
 
         self.textBrowser = QTextBrowser(Widget)
         self.textBrowser.setObjectName(u"textBrowser")
-        self.textBrowser.setGeometry(QRect(10, 10, 651, 511))
+        self.textBrowser.setGeometry(QRect(10, 10, 650, 500))
         font = QFont()
         font.setFamilies([u"Arial"])
-        font.setPointSize(13)
+        font.setPointSize(14)
         self.textBrowser.setFont(font)
         self.textBrowser.setStyleSheet(
-            "QTextBrowser {padding: 10px; border: 1px solid black;}")
+            u"border: 1px solid gray; text-indent: 10px; line-height: 1.2;")
         Ui_Widget.component.append(self.textBrowser)
 
         self.scrollArea = QScrollArea(Widget)
         self.scrollArea.setObjectName(u"scrollArea")
-        self.scrollArea.setGeometry(QRect(670, 10, 120, 511))
+        self.scrollArea.setGeometry(QRect(670, 10, 120, 500))
         self.scrollArea.setWidgetResizable(True)
         self.scrollAreaWidgetContents = QWidget()
         self.scrollAreaWidgetContents.setObjectName(
             u"scrollAreaWidgetContents")
-        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 118, 509))
+        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 120, 500))
         self.scrollArea.setWidget(self.scrollAreaWidgetContents)
         Ui_Widget.component.append(self.scrollArea)
 
@@ -53,14 +175,14 @@ class Ui_Widget(object):
         self.scrollAreaWidgetContents = QWidget()
         self.scrollAreaWidgetContents.setObjectName(
             u"scrollAreaWidgetContents")
-        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 118, 509))
+        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 120, 500))
         self.scrollArea.setWidget(self.scrollAreaWidgetContents)
         Ui_Widget.component.append(self.scrollAreaWidgetContents)
 
         # SEND BUTTON
         self.pushButton = QPushButton(Widget)
         self.pushButton.setObjectName(u"pushButton")
-        self.pushButton.setGeometry(QRect(680, 540, 83, 29))
+        self.pushButton.setGeometry(QRect(680, 540, 80, 35))
         Ui_Widget.component.append(self.pushButton)
 
         self.retranslateUi(Widget)
@@ -112,10 +234,6 @@ class Widget(QWidget):
         self.ui.plainTextEdit.installEventFilter(self)
 
 
-# create a widget object
-app = QApplication(sys.argv)
-widget = Widget()
-
 # ----------------------------------------------------------TCP Socket Programming----------------------------------------------------------
 
 
@@ -124,51 +242,43 @@ def receive():
     while True:
         try:
             message = client.recv(1024).decode('utf-8')
-            widget.ui.textBrowser.append(message)
+            chat_room.ui.textBrowser.append(message)
         except:
-            ctypes.windll.user32.MessageBoxW(
-                0, "You have been disconnected from the server", "Notification", 0)
             client.close()
             break
 
+
+# --------------------------------------------------------------Global Variables--------------------------------------------------------------
 # create a socket object
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+app = QApplication(sys.argv)
+chat_room = Widget()
 
 # ----------------------------------------------------------Main----------------------------------------------------------
 if __name__ == "__main__":
 
-    # USAGE: python client.py <host> <port>
-    if len(sys.argv) != 3:
-        print("Usage: python client.py <host> <port>")
-        sys.exit(1)
-
-    # get host and port from command line
-    host = sys.argv[1]
-    port = int(sys.argv[2])
-
-    # connect to the server
-    try:
-        client.connect((host, port))
-    except:
-        ctypes.windll.user32.MessageBoxW(
-            0, "Cannot connect to the server", "Error", 0)
-        sys.exit(1)
-
+    # create a login widget
+    login = Login()
+    login.show()
+    
     # request and store nickname
-    nickname = input('Choose your nickname: ')
-    client.send(nickname.encode('utf-8'))
-    message = client.recv(1024).decode('utf-8')
-    while message == 'RESEND_NICK':
-        nickname = input('Nickname already in use. Choose another: ')
-        client.send(nickname.encode('utf-8'))
-        message = client.recv(1024).decode('utf-8')
+    # nickname = input('Choose your nickname: ')
+    # client.send(nickname.encode('utf-8'))
+    # message = client.recv(1024).decode('utf-8')
+    # while message == 'RESEND_NICK' or message == "INVALID_NICK":
+    #     if message == "INVALID_NICK":
+    #         nickname = input('Invalid nickname. Choose another: ')
+    #     else:
+    #         nickname = input('Nickname already in use. Choose another: ')
+    #     client.send(nickname.encode('utf-8'))
+    #     message = client.recv(1024).decode('utf-8')
 
-    # start threads for listening and writing
-    receive_thread = threading.Thread(target=receive)
-    receive_thread.start()
+    # # start threads for listening and writing
+    # receive_thread = threading.Thread(target=receive)
+    # receive_thread.start()
 
-    # start the widget
-    widget.ui.set_state(True)
-    widget.show()
+    # # start the widget
+    # widget.ui.set_state(True)
+    # widget.show()
     app.aboutToQuit.connect(lambda: client.close())
     sys.exit(app.exec())
--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 # build a chat server using socket programming
 import socket
 import threading
-
+import re
 # CLIENT[NICKNAME] = [SOCKET, ADDRESS]
 CLIENTS = dict()
 
@@ -19,14 +19,17 @@ server.bind((host, port))
 # listen for clients
 server.listen()
 
+# send welcome message to new client
+
 
 def welcome(client_socket):
-    # print all existing clients
-    client_socket.send('Welcome to the chatroom!\n'.encode('utf-8'))
-    client_socket.send('Existing clients: '.encode('utf-8'))
-    for nickname in CLIENTS:
-        client_socket.send((nickname+" ").encode('utf-8'))
-    client_socket.send('\n--------------Start chatting now!\n'.encode('utf-8'))
+    broadcast('Welcome to the chatroom!', "SERVER")
+    broadcast('Start chatting now!', "SERVER")
+    # broadcast('Existing clients: '.encode('utf-8'), "SERVER")
+    # for nickname in CLIENTS:
+    #     client_socket.send((nickname+" ").encode('utf-8'))
+
+# send private message to a client
 
 
 def private_message(client_socket, nickname, message):
@@ -40,7 +43,7 @@ def private_message(client_socket, nickname, message):
         client_socket.send('User not found. Please try again.'.encode('utf-8'))
         return
     send_to_client(CLIENTS[message[1]][0],
-                   f'Private {nickname}: {message[2]}')
+                   f'Private [{nickname}]: {message[2]}')
 
 
 def send_to_client(client_socket, message):
@@ -48,15 +51,27 @@ def send_to_client(client_socket, message):
 
 
 # broadcast messages to all clients
+
+
 def broadcast(message, nickname, sender=None):
     # notification message
     if sender is None:
+        # format message to --- message --- that does not exceed 60 characters
+        notification = " "+(59-len(message))//2 * '-' + \
+            message + (59-len(message))//2 * '-' + '\n'
+        if len(notification) > 60:
+            tmp = message.split(" ", 1)
+            notification = " "+(59-len(tmp[0]))//2 * '-' + \
+                tmp[0] + (59-len(tmp[0]))//2 * '-' + '\n' + \
+                " "+(59-len(tmp[1]))//2 * '-' + \
+                tmp[1] + (59-len(tmp[1]))//2 * '-' + '\n'
+        notification = notification.encode('utf-8')
         for client_socket, address in CLIENTS.values():
-            client_socket.send(message)
+            client_socket.send(notification)
     else:
         for client_socket, address in CLIENTS.values():
             if client_socket is not sender:
-                client_socket.send(f'{nickname}: '.encode('utf-8') + message)
+                client_socket.send(f'[{nickname}]: '.encode('utf-8') + message)
 
 # handle client messages
 
@@ -76,32 +91,54 @@ def handle(client_socket, nickname):
             # remove client from CLIENTS
             del CLIENTS[nickname]
             # notify to all clients
-            broadcast(f'{nickname} left the chatroom!'.encode(
-                'utf-8'), nickname)
+            broadcast(f'{nickname} left the chatroom!', "SERVER")
             break
+
+# validate nickname
+
+
+def validate_nickname(client_socket) -> str:
+    nickname = client_socket.recv(1024).decode('utf-8')
+    valid_pattern = re.compile(
+        r'^(?=.{2,16}$)(?![_.])(?!.*[_.]{2})(?!.*[\s]{2})[a-zA-Z0-9._\s]+(?<![_.])$')
+    # username is 2-16 characters long
+    # no _ or . or whitespace at the beginning
+    # no __ or _. or ._ or .. or double whitespace inside
+    # allowed characters
+    # no _ or . or whitespace at the end
+    if not valid_pattern.match(nickname):
+        client_socket.send('INVALID_NICK'.encode('utf-8'))
+        return validate_nickname(client_socket)
+    if nickname in CLIENTS:
+        client_socket.send('RESEND_NICK'.encode('utf-8'))
+        return validate_nickname(client_socket)
+    return nickname
+
 
 # handle new connections
 
 
 def on_connect(client_socket, address):
     # request and store nickname
-    nickname = client_socket.recv(1024).decode('utf-8')
-    while nickname in CLIENTS:
-        client_socket.send('RESEND_NICK'.encode('utf-8'))
-        nickname = client_socket.recv(1024).decode('utf-8')
-    # store client information
-    CLIENTS[nickname] = (client_socket, address)
-    # notify to all clients
-    broadcast(f'{nickname} joined the chatroom!'.encode(
-        'utf-8'), nickname)
-    # welcome new client
-    welcome(client_socket)
-    # start thread for handling client
-    thread = threading.Thread(target=handle, args=(client_socket, nickname))
-    thread.start()
-
+    try:
+        nickname = validate_nickname(client_socket)
+        # store client information
+        CLIENTS[nickname] = (client_socket, address)
+        # notify to all clients
+        broadcast(f'{nickname} joined the chatroom!', "SERVER")
+        # welcome new client
+        welcome(client_socket)
+        # start thread for handling client
+        thread = threading.Thread(
+            target=handle, args=(client_socket, nickname))
+        thread.start()
+    except:
+        client_socket.close()
+        return
 
 # start accepting clients
+
+
 def accept():
     while True:
         try:
@@ -113,32 +150,31 @@ def accept():
         except:
             break
 
+
 if __name__ == '__main__':
     import tkinter as tk
     from tkinter import messagebox
     import sys
     root = tk.Tk()
+
     def on_closing():
-        if messagebox.askokcancel("Quit", "Do you want to quit?"):
-            root.destroy()
-            server.close()
-            sys.exit(0)
+        root.destroy()
+        server.close()
+        sys.exit(0)
 
     root.protocol("WM_DELETE_WINDOW", on_closing)
 
     # Set the window's position
-    root.geometry(f"{300}x{100}+{(root.winfo_screenwidth() - 300) // 2}+{(root.winfo_screenheight() - 100) // 2}")
-    #set the title
+    root.geometry(
+        f"{300}x{100}+{(root.winfo_screenwidth() - 300) // 2}+{(root.winfo_screenheight() - 100) // 2}")
+    # set the title
     root.title("SERVER IS ONLINE!")
-    
-    #display host and port on tkinter windows (centered)
+
+    # display host and port on tkinter windows (centered)
     tk.Label(root, text=f"HOST: {host}").pack(pady=10)
     tk.Label(root, text=f"PORT: {port}").pack(pady=10)
-    
-    #start accepting clients
+
+    # start accepting clients
     accept_thread = threading.Thread(target=accept)
     accept_thread.start()
     root.mainloop()
-
-
-    
